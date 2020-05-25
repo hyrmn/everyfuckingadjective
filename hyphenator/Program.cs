9b3d721c84c2fbd1c2407b4c01f95a1f3d89f2c6
Ben Hyrman <ben.hyrman@gmail.com>
2020-05-25 00:45:42 -0400

    Code to hyphenate the adjectives
    This code assumes there is one word per line (although it work with more).
    It will write out each line with the hyphenation applied.
    
    So, pipe the output to another file to save it.
    
    Usage to generate the hyphenated list
    
    ```powershell
    dotnet run ../data/adjectives.txt > ../data/hyphenated-adjectives.txt
    ```

---
+++ hyphenator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using NHyphenator;
+using NHyphenator.Loaders;
+
+namespace hyphenator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Usage:\n\thyphenator \"path\\to\\file.txt\"");
+                    return;
+                }
+
+                var opts = new ResourceHyphenatePatternsLoader(HyphenatePatternsLanguage.EnglishUs);
+                var nhyphenator = new Hyphenator(opts, "-");
+
+                try
+                {
+                    var lines =  File.ReadAllLines(args[0]);
+                    foreach(var line in lines) 
+                    {
+                        Console.WriteLine(nhyphenator.HyphenateText(line));
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine($"Could not find {args[0]}. Check the file path.");
+                }
+        }
+    }
+}
