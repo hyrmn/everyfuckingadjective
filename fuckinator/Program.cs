81491ece3989ea6e0e970e692f02f7a14c2b90eb
Ben Hyrman <ben.hyrman@gmail.com>
2020-05-25 01:01:05 -0400

    Make up the fucking list
    This takes in the file created by the `hyphenator` and then
    writes a line to the console for every line in the input file.
    
    It looks for the first hyphen and inserts `fucking-` after.
    It then removes all of the remaining hyphens in the word.
    
    We need some rules to handle things like if the last hyphened group
    is an `-ing` then we don't want to add `fucking-` immediately before that.
    It looks weird.
    
    To use
    
    ```powershell
    dotnet run ../data/hyphenated-adjectives.txt > ../data/fucking-adjectives.txt
    ```

---
+++ fuckinator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace fuckinator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Usage:\n\fuckinator \"path\\to\\file.txt\"");
+                    return;
+                }
+
+                try
+                {
+                    var lines =  File.ReadAllLines(args[0]);
+                    foreach(var line in lines) 
+                    {
+                        if(!line.Contains("-")) 
+                        {
+                            Console.WriteLine($"fucking {line}");
+                        }
+                        else
+                        {
+                            Console.WriteLine(InsertFuck(line));
+                        }
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine($"Could not find {args[0]}. Check the file path.");
+                }
+        }
+
+        private static string InsertFuck(string line)
+        {
+            // We'll insert 'fucking-' after the first hyphen.
+            // We'll then remove the rest of the hyphens because we only want to emphasize the fucking insertion
+            var preIdx = line.IndexOf("-");
+            var rest = line.Substring(preIdx + 1).Replace("-", "");
+            var theFuckingVersion = line.Substring(0, preIdx+1) + "fucking-" + rest;
+            
+            return theFuckingVersion;
+        }
+    }
+}
